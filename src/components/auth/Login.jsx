ffe6b0542855993d1ef8058666fc67661f2869ef
manish3-4 <preetisikka553@gmail.com>
2025-03-24 16:34:45 +0530

    ready

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -69,7 +69,7 @@ const Login = () => {
                 name="password"
                 type="password"
                 required
-                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none relative block w-full px-3 py-2 border border-gray-300 bg-amber-50 placeholder-gray-500 text-gray-900 rounded focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
                 value={formData.password}
                 onChange={handleChange}
